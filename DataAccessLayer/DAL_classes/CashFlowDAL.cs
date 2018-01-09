d3d2aefa96aa2fc018d69ede6ff47092fa4cb723
RamananSri <1058515@ucn.dk>
2018-01-09 09:44:04 +0100

    cashflow

--- DataAccessLayer/DAL_classes/CashFlowDAL.cs
+++ DataAccessLayer/DAL_classes/CashFlowDAL.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using DataAccessLayer.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ModelLayer;
 
 namespace DataAccessLayer.DAL_classes
 {
-    public class CashFlowDAL
+    public class CashFlowDAL : ICashFlowDAL
     {
+        public CashFlowDAL()
+        {
+
+        }
+
+        public void DeleteCashFlow(int id)
+        {
+            try
+            {
+                using (DomainContext ctx = new DomainContext())
+                {
+                    
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+
+        public CashFlow GetCashFlow(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PostCashFlow(CashFlow cashflow)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PutCashFlow(CashFlow cashflow)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ DataAccessLayer/Interfaces/ICashFlowDAL.cs
@@ -0,0 +1,18 @@
+﻿using ModelLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Interfaces
+{
+    interface ICashFlowDAL
+    {
+        List<CashFlow>
+        CashFlow GetCashFlow(int id);
+        void PostCashFlow(CashFlow cashflow);
+        void DeleteCashFlow(int id);
+        void PutCashFlow(CashFlow cashflow);
+    }
+}
